1f6e129a12f455f82bb054bf6c349da46c7ff0a7
[Laiba Irshd] <[laibairshad3007@gmail.com]>
2024-12-01 01:20:31 -0800

    Add .env file and update db.py

--- db.py
+++ db.py
@@ -1,14 +1,13 @@
 import psycopg2
-from psycopg2 import sql
-from psycopg2.extras import RealDictCursor
+from decouple import config
 
 def connect_db():
     conn=psycopg2.connect(
-        host="localhost",
-        database="Password-Generator",
-        user="postgres",
-        password="libs_3007",
-        port = "5432"
+       host=config("DB_HOST"),
+       database=config("DB_NAME"),
+       user=config("DB_USER"),
+       password=config("DB_PASSWORD"),
+       port=config("DB_PORT"),
     )
     return conn
 
